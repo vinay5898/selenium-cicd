2afb72c49381d9567e8005e2fafae591dd03af3f
vinay-at-557485523436 <vinaykumargalove@gmail.com>
2022-05-31 14:38:20 +0530

    selenium

---
+++ install_driver.sh
@@ -0,0 +1,14 @@
+# Download and Install chromedriver
+wget -N https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
+
+
+# Install chrome broswer
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+apt-get -y update
+apt-get -y install google-chrome-stable
