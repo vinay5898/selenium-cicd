9481511b12bcba3123efb2d0d47685f53768dd04
vinay5898 <69567526+vinay5898@users.noreply.github.com>
2022-05-31 16:55:34 +0530

    Update install_driver.sh

--- install_driver.sh
+++ install_driver.sh
@@ -1,5 +1,5 @@
 # Download and Install chromedriver
-wget -N https://chromedriver.storage.googleapis.com/102.0.5005.61 /chromedriver_linux64.zip -P ~/
+wget -N https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
